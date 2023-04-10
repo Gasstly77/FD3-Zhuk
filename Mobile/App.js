8252b737da7a2801f79dcb2a78d916a688f4007f
Sergey Zhuk <zhuksiarhei93@gmail.com>
2023-04-10 21:36:34 +0300

    Hometask Mobile

---
+++ Mobile/App.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MobileCompany from './components/MobileCompany/MobileCompany';
+
+let clientsArr = [
+  { id: 101, FIO: { fam: "Иванов", im: "Иван", otch: "Иванович" }, balance: 200 },
+  { id: 105, FIO: { fam: "Сидоров", im: "Сидор", otch: "Сидорович" }, balance: 250 },
+  { id: 110, FIO: { fam: "Петров", im: "Пётр", otch: "Петрович" }, balance: 180 },
+  { id: 120, FIO: { fam: "Григорьев", im: "Григорий", otch: "Григорьевич" }, balance: -220 },
+];
+
+ReactDOM.render(
+  <MobileCompany
+    clients={clientsArr}
+  />
+  , document.getElementById('container')
+);
+
